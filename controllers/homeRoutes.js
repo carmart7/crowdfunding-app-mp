e110ccd6585de5730678128b98d518d201bbbff8
Kevin Xu <kevinpxu21@gmail.com>
2022-11-07 16:10:31 -0600

    added login and new user routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,17 +2,25 @@ const router = require('express').Router();
 const { Project } = require('../models');
 
 router.get('/', async (req, res) => {
-    const projectData = await Project.findAll();
-    const projects = projectData.map((project) => project.get({ plain: true }));
-    res.render('homepage', {
-        projects
-    });
+  const projectData = await Project.findAll();
+  const projects = projectData.map((project) => project.get({ plain: true }));
+  res.render('homepage', {
+    projects,
+  });
 });
 
 router.get('/projects/:id', async (req, res) => {
-    const projectData = await Project.findByPk(req.params.id);
-    const projects = [ projectData.get({ plain: true }) ];
-    res.render('homepage', { projects });
-})  
+  const projectData = await Project.findByPk(req.params.id);
+  const projects = [projectData.get({ plain: true })];
+  res.render('homepage', { projects });
+});
+
+router.get('/login', async (req, res) => {
+  res.render('login');
+});
+
+router.get('/login/new', async (req, res) => {
+  res.render('newUser');
+});
 
 module.exports = router;
